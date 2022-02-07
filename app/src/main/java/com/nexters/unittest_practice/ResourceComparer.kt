a7442df713e5a67e17975b8bd53b25d14f4be18d
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2022-02-07 22:42:38 +0900

    [Add] @Before @After

---
+++ app/src/main/java/com/nexters/unittest_practice/ResourceComparer.kt
@@ -0,0 +1,11 @@
+package com.nexters.unittest_practice
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
+
+}
